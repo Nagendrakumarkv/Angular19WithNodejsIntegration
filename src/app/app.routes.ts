0cb8122dac5173a79b2d5e750e9cc0c076cee658
Nagendra Kumar K V <nagendrakumarkv123@gmail.com>
2025-04-21 09:34:10 +0530

    Set up Angular 19 frontend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/messages', pathMatch: 'full' },
+  {
+    path: 'messages',
+    loadComponent: () =>
+      import('./features/messages/messages.component').then(
+        (m) => m.MessagesComponent
+      ),
+  },
+];
